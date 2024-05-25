62515f4b2ff0af0d3ef4c54c0f49d18b84dcbea9
hanheel <hanhee630630@gmail.com>
2024-05-26 00:50:21 +0900

    Header(nav)구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import styles from "./App.module.css";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <div>App</div>;
+  return (
+  <div className={styles.wrap}>
+    <Header />
+  </div>
+  )
 }
 
 export default App;
