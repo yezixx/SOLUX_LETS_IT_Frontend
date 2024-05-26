1d7c6d74b1069ed6e493b50dd9b3a563906e215a
hanheel <hanhee630630@gmail.com>
2024-05-26 15:29:41 +0900

    Organize navCont

--- src/Components/Header/Nav2/Nav2.jsx
+++ src/Components/Header/Nav2/Nav2.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import styles from './Nav2.module.css';
 
 const Nav2 = () => {
+
+    const navCont = ['프로젝트 찾기', '내 프로젝트', '공모전/뉴스레터', '렛츠IT 커뮤니티']
     const subNavCont = {
         '프로젝트 찾기': ['전체 프로젝트', '지역으로 찾기', '분야로 찾기', '내 맞춤 프로젝트'],
         '내 프로젝트': ['구인 중인 프로젝트', '진행 중인 프로젝트', '스크랩한 프로젝트'],
-        '마이페이지': []
     };
 
     const [hoverState, setHoverState] = useState({
@@ -14,6 +15,7 @@ const Nav2 = () => {
         '공모전/뉴스레터': false,
         '렛츠IT 커뮤니티': false
     });
+    //Nav / subNav data
 
     const handleMouseEnter = (menu) => {
         setHoverState(prevState => ({
@@ -28,11 +30,12 @@ const Nav2 = () => {
             [menu]: false
         }));
     };
+    //mouseEnter, mouseLeave시 hover state 설정
 
     return (
         <div className={styles.Nav2Wrap}>
             <div className={styles.Nav2Cont}>
-                {['프로젝트 찾기', '내 프로젝트', '공모전/뉴스레터', '렛츠IT 커뮤니티']
+                {navCont
                     .map((menu, id) => (
                         <div key={id} onMouseEnter={() => handleMouseEnter(menu)} onMouseLeave={() => handleMouseLeave(menu)}>
                             <button>{menu}</button>
