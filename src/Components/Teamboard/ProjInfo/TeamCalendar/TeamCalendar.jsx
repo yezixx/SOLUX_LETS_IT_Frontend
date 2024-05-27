6f48dd0a9fa39adced3bfb9c2b8035c74b171670
yezixx <skylow917@daum.net>
2024-05-27 11:44:14 +0900

    FullCalendar 렌더링

--- src/Components/Teamboard/ProjInfo/TeamCalendar/TeamCalendar.jsx
+++ src/Components/Teamboard/ProjInfo/TeamCalendar/TeamCalendar.jsx
@@ -1,9 +1,22 @@
 import styles from "./TeamCalendar.module.css";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import FullCalendar from "@fullcalendar/react";
 
 const TeamCalendar = () => {
   return (
     <div className={styles.TeamCalendarWrap}>
-      <div>팀 캘린더</div>
+      <div>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView={"dayGridMonth"}
+          headerToolbar={{
+            end: "today,prev,next",
+          }}
+          height={"600px"}
+        />
+      </div>
     </div>
   );
 };
