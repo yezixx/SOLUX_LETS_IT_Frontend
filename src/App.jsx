d8edffd3803231c6c8813cf3b7a57347e0d4d6be
yezixx <skylow917@daum.net>
2024-05-27 11:29:22 +0900

    팀게시판 컴포넌트 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import styles from "./App.module.css";
 import Header from "./Components/Header/Header";
+import Teamboard from "./Components/Teamboard/Teamboard";
 
 function App() {
   return (
-  <div className={styles.wrap}>
-    <Header />
-  </div>
-  )
+    <div className={styles.wrap}>
+      <Header />
+      <Teamboard />
+    </div>
+  );
 }
 
 export default App;
