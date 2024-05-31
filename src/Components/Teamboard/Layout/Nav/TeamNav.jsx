eee3a166d59450728dc70e91507411f18b63ec21
yezixx <skylow917@daum.net>
2024-05-31 22:18:05 +0900

    position 속성 대신 flex와 margin 사용

--- src/Components/Teamboard/Layout/Nav/TeamNav.jsx
+++ src/Components/Teamboard/Layout/Nav/TeamNav.jsx
@@ -10,33 +10,35 @@ const TeamNav = () => {
     <div className={styles.nav_wrapper}>
       <div className={styles.nav_container}>
         <div className={styles.nav_label}>학원 청구 정산 서비스</div>
-        <Link to={"/"}>
-          <button
-            className={`${styles.nav_button} ${
-              currentUrl === "/" ? styles.currentPage_btn : ""
-            }`}
-          >
-            프로젝트 정보
-          </button>
-        </Link>
-        <Link to={"/SetMember"}>
-          <button
-            className={`${styles.nav_button} ${
-              currentUrl === "/SetMember" ? styles.currentPage_btn : ""
-            }`}
-          >
-            팀원 설정
-          </button>
-        </Link>
-        <Link to={"/ManageProj"}>
-          <button
-            className={`${styles.nav_button} ${
-              currentUrl === "/ManageProj" ? styles.currentPage_btn : ""
-            }`}
-          >
-            프로젝트 관리
-          </button>
-        </Link>
+        <div className={styles.nav_buttons_container}>
+          <Link to={"/"}>
+            <button
+              className={`${styles.nav_button} ${
+                currentUrl === "/" ? styles.currentPage_btn : ""
+              }`}
+            >
+              프로젝트 정보
+            </button>
+          </Link>
+          <Link to={"/SetMember"}>
+            <button
+              className={`${styles.nav_button} ${
+                currentUrl === "/SetMember" ? styles.currentPage_btn : ""
+              }`}
+            >
+              팀원 설정
+            </button>
+          </Link>
+          <Link to={"/ManageProj"}>
+            <button
+              className={`${styles.nav_button} ${
+                currentUrl === "/ManageProj" ? styles.currentPage_btn : ""
+              }`}
+            >
+              프로젝트 관리
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
