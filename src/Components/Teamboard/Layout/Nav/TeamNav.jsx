2480269ce2755a0fd93f606744bafa67dda93067
yezixx <skylow917@daum.net>
2024-05-31 21:57:50 +0900

    현재 페이지에 따라 버튼 색상 적용

--- src/Components/Teamboard/Layout/Nav/TeamNav.jsx
+++ src/Components/Teamboard/Layout/Nav/TeamNav.jsx
@@ -11,13 +11,31 @@ const TeamNav = () => {
       <div className={styles.nav_container}>
         <div className={styles.nav_label}>학원 청구 정산 서비스</div>
         <Link to={"/"}>
-          <button>프로젝트 정보</button>
+          <button
+            className={`${styles.nav_button} ${
+              currentUrl === "/" ? styles.currentPage_btn : ""
+            }`}
+          >
+            프로젝트 정보
+          </button>
         </Link>
         <Link to={"/SetMember"}>
-          <button>팀원 설정</button>
+          <button
+            className={`${styles.nav_button} ${
+              currentUrl === "/SetMember" ? styles.currentPage_btn : ""
+            }`}
+          >
+            팀원 설정
+          </button>
         </Link>
         <Link to={"/ManageProj"}>
-          <button>프로젝트 관리</button>
+          <button
+            className={`${styles.nav_button} ${
+              currentUrl === "/ManageProj" ? styles.currentPage_btn : ""
+            }`}
+          >
+            프로젝트 관리
+          </button>
         </Link>
       </div>
     </div>
