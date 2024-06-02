0afb24a1af640bc328aeeafc0277543e567eaad0
hanheel <hanhee630630@gmail.com>
2024-06-03 04:32:34 +0900

    세부 CSS 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import styles from "./App.module.css";
 import Header from "./Components/Header/Header";
-
+import MyProfile from "./Screen/MyPage/Profil/MyProfile";
 function App() {
   return (
   <div className={styles.wrap}>
     <Header />
+    <div className={styles.main}> 
+    <MyProfile /> {/*라우팅 시 outlet이 들어갈 부분*/}
+    </div>
   </div>
   )
 }
