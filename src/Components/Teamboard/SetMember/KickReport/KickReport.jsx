4da7309727931b5dca4b6205ca1f301b5ef5e2ff
yezixx <skylow917@daum.net>
2024-06-03 10:32:47 +0900

    Feat: 강퇴 및 신고 컴포넌트 생성

---
+++ src/Components/Teamboard/SetMember/KickReport/KickReport.jsx
@@ -0,0 +1,11 @@
+import styles from "./KickReport.module.css";
+
+const KickReport = () => {
+  return (
+    <div className={styles.kick_report__wrapper}>
+      <div>강퇴 및 신고</div>
+    </div>
+  );
+};
+
+export default KickReport;
