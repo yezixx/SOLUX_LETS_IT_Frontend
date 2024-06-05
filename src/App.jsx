d3930e2fc10fe9fa3d7b4012c0fd8ae11e0e72c9
yezixx <skylow917@daum.net>
2024-06-05 12:42:46 +0900

    Feat: feature branch pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import styles from "./App.module.css";
 import Header from "./Components/Header/Header";
-<<<<<<< HEAD
 import { RouterInfo } from "./Components/RouterInfo";
 
 const RouterObject = createBrowserRouter(RouterInfo);
@@ -13,18 +12,6 @@ function App() {
       <RouterProvider router={RouterObject} />
     </div>
   );
-=======
-import MyProfile from "./Screen/MyPage/Profil/MyProfile";
-function App() {
-  return (
-  <div className={styles.wrap}>
-    <Header />
-    <div className={styles.main}> 
-    <MyProfile /> {/*라우팅 시 outlet이 들어갈 부분*/}
-    </div>
-  </div>
-  )
->>>>>>> 24b2e8beac2e34a90323d6d328e08e971cc21025
 }
 
 export default App;
