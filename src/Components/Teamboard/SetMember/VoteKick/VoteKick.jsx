a9c466438f44a74e95182b69a46342bd51f88a9a
yezixx <skylow917@daum.net>
2024-06-13 11:30:23 +0900

    Feat; - 더미데이터 순회하면서 렌더링

--- src/Components/Teamboard/SetMember/VoteKick/VoteKick.jsx
+++ src/Components/Teamboard/SetMember/VoteKick/VoteKick.jsx
@@ -1,11 +1,17 @@
-import KickMember from "../KickMember/KickMember";
+import KickMember from "./VoteKickMember/VoteKickMember";
 import styles from "./VoteKick.module.css";
 
 const Votekick = () => {
+  const voteKickmembers = [{ id: 1, name: "Tom BE" }];
+
   return (
     <div className={styles.voteKick}>
-      <div className={styles.voteKick}>강퇴 투표</div>
-      <KickMember />
+      <div className={styles.voteKick__label}>강퇴 투표</div>
+      <div className={styles.voteKick__container}>
+        {voteKickmembers.map((member) => (
+          <KickMember key={member.id} memberName={member.name} />
+        ))}
+      </div>
     </div>
   );
 };
