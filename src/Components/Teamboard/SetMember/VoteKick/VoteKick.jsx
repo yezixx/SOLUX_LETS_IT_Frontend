4798d8187954f67aefe2ca2ecfc0baa52f6b5411
yezixx <skylow917@daum.net>
2024-06-13 10:54:58 +0900

    Feat: 강퇴 투표 중인 팀원 컴포넌트 렌더링

--- src/Components/Teamboard/SetMember/VoteKick/VoteKick.jsx
+++ src/Components/Teamboard/SetMember/VoteKick/VoteKick.jsx
@@ -1,5 +1,13 @@
+import KickMember from "../KickMember/KickMember";
+import styles from "./VoteKick.module.css";
+
 const Votekick = () => {
-  return <div>강퇴 투표</div>;
+  return (
+    <div className={styles.voteKick}>
+      <div className={styles.voteKick}>강퇴 투표</div>
+      <KickMember />
+    </div>
+  );
 };
 
 export default Votekick;
