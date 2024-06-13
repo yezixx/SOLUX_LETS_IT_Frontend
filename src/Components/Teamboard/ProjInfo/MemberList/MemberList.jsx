b2f1851eb4a12f6d0de121a7126ea3eac5587aa8
yezixx <skylow917@daum.net>
2024-06-13 11:03:24 +0900

    Rename: MemberItem 폴더 이동으로 인한 경로 수정

--- src/Components/Teamboard/ProjInfo/MemberList/MemberList.jsx
+++ src/Components/Teamboard/ProjInfo/MemberList/MemberList.jsx
@@ -1,5 +1,5 @@
 import styles from "./MemberList.module.css";
-import MemberItem from "../MemberItem/MemberItem";
+import MemberItem from "./MemberItem/MemberItem";
 
 const MemberList = () => {
   const members = [
