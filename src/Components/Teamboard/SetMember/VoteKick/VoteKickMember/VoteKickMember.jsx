f23eae240b068fced98abaa0d106f659c99bd263
yezixx <skylow917@daum.net>
2024-06-13 11:59:59 +0900

    Feat: 컴포넌트 수정

--- src/Components/Teamboard/SetMember/VoteKick/VoteKickMember/VoteKickMember.jsx
+++ src/Components/Teamboard/SetMember/VoteKick/VoteKickMember/VoteKickMember.jsx
@@ -1,13 +1,15 @@
-import styles from "./KickMember.module.css";
+import styles from "./VoteKickMember.module.css";
 
 const KickMember = ({ memberName }) => {
   return (
-    <div className={styles.kickMember}>
-      <div className={styles.kickMember_container}>
-        <button className={styles.kickMember__button}>
-          <div className={styles.kickMember__profilePic}>사진</div>
-          <div className={styles.kickMember__name}>{memberName}</div>
-        </button>
+    <div className={styles.voteKickMember}>
+      <div className={styles.voteKickMember_container}>
+        <div className={styles.voteKickMember__profilePic}>사진</div>
+        <div className={styles.voteKickMember__name}>{memberName}</div>
+        <div className={styles.voteKickMember__button}>
+          <button className={styles["voteKickMember__button--o"]}>O</button>
+          <button className={styles["voteKickMember__button--x"]}>X</button>
+        </div>
       </div>
     </div>
   );
