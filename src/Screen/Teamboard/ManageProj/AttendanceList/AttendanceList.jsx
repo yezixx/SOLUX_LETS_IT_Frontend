caa006a3f7eb220982a9007e4e14b4b05542157c
yezixx <skylow917@daum.net>
2024-06-20 17:52:38 +0900

    Feat: - AttendanceItem 렌더링 - 더미데이터 사용

--- src/Screen/Teamboard/ManageProj/AttendanceList/AttendanceList.jsx
+++ src/Screen/Teamboard/ManageProj/AttendanceList/AttendanceList.jsx
@@ -1,7 +1,28 @@
 import styles from "./AttendanceList.module.css";
+import AttendanceItem from "./AttendanceItem/AttendanceItem";
 
 const AttendanceList = () => {
-  return <div>회의 참석 인증</div>;
+  const meetings = [
+    { id: 1, date: "2024-04-01", isCompleted: true },
+    { id: 2, date: "2024-04-06", isCompleted: true },
+    { id: 3, date: "2024-04-06", isCompleted: false },
+  ];
+  return (
+    <div>
+      회의 참석 인증
+      <div>
+        {meetings.map((meeting) => (
+          <AttendanceItem
+            key={meeting.id}
+            id={meeting.id}
+            date={meeting.date}
+            isCompleted={meeting.isCompleted}
+            length={meetings.length}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AttendanceList;
