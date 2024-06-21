012d5c7073946be5bcae09c14c7e790e0e8465c2
yezixx <skylow917@daum.net>
2024-06-21 13:47:59 +0900

    Feat: AttendanceItem UI 구현

--- src/Screen/Teamboard/ManageProj/AttendanceList/AttendanceItem/AttendanceItem.jsx
+++ src/Screen/Teamboard/ManageProj/AttendanceList/AttendanceItem/AttendanceItem.jsx
@@ -1,7 +1,28 @@
+import CheckIcon from "../../../../../Image/Icons/CheckIcon";
 import styles from "./AttendanceItem.module.css";
 
-const AttendanceItem = () => {
-  return <div>회의 참석 리스트 아이템</div>;
+const AttendanceItem = ({ id, date, isCompleted, length }) => {
+  const classNames = `
+    ${styles.attendanceItem__block}
+    ${id === 1 ? styles["attendanceItem__block--first"] : ""}
+    ${id === length ? styles["attendanceItem__block--last"] : ""}
+  `;
+
+  return (
+    <div className={styles.attendanceItem.trim()}>
+      <div className={classNames}>
+        <div className={styles.attendanceItem__icon}>
+          <CheckIcon
+            width="20px"
+            height="20px"
+            color={isCompleted ? "var(--main-color2)" : "var(--text-color2)"}
+          />
+        </div>
+        <div className={styles.attendanceItem__text}>{id} 번째 인증</div>
+        <div className={styles.attendanceItem__date}>{date}</div>
+      </div>
+    </div>
+  );
 };
 
 export default AttendanceItem;
