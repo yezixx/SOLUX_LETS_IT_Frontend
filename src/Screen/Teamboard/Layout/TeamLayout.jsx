bc1ad81eabb71979a55c39ef604fe77a250786e4
yezixx <skylow917@daum.net>
2024-06-21 17:28:19 +0900

    Refactor: TopLabel 컴포넌트 대신 일반 태그 사용

--- src/Screen/Teamboard/Layout/TeamLayout.jsx
+++ src/Screen/Teamboard/Layout/TeamLayout.jsx
@@ -1,12 +1,10 @@
 import styles from "./TeamLayout.module.css";
-import TopLabel from "./TopLabel/TopLabel";
 import TeamNav from "./Nav/TeamNav";
 import TeamContainer from "./Container/TeamContainer";
 
 const TeamLayout = () => {
   return (
     <div className={styles.teamLayout}>
-      <TopLabel />
       <TeamNav />
       <TeamContainer />
     </div>
--- src/Screen/Teamboard/Teamboard.jsx
+++ src/Screen/Teamboard/Teamboard.jsx
@@ -4,6 +4,7 @@ import TeamLayout from "./Layout/TeamLayout";
 const Teamboard = () => {
   return (
     <div className={styles.teamboard}>
+      <div className={styles.teamboard__label}>팀 게시판</div>
       <TeamLayout />
     </div>
   );
