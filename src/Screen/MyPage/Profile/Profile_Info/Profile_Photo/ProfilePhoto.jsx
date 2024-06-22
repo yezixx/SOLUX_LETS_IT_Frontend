b50286379ce39fb579038d0d3b952e93884708f6
hanheel <hanhee630630@gmail.com>
2024-06-22 20:42:52 +0900

    feature : 내 프로필 화면 제작

--- src/Screen/MyPage/Profile/Profile_Info/Profile_Photo/ProfilePhoto.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './ProfilePhoto.module.css'
-
-const ProfilePhoto = ()=>{
-    return(
-        <div className={styles.myProfile__photo}>프로필 사진</div>
-    )
-}
-
-export default ProfilePhoto
