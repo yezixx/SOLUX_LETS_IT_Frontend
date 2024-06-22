ef9a6c373c12b4ea8efa971d5523d6d2ec1725a5
hanheel <hanhee630630@gmail.com>
2024-06-22 14:53:38 +0900

    fix : App.jsx css 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import styles from "./App.module.css";
 import Header from "./Components/Header/Header";
-import MyProfile from "./Screen/MyPage/Profil/MyProfile";
+import MyProfile from "./Screen/MyPage/Profile/Profile";
 function App() {
   return (
   <div className={styles.wrap}>
     <Header />
     <div className={styles.main}> 
-    <MyProfile /> {/*라우팅 시 outlet이 들어갈 부분*/}
+    <MyProfile /> {/*라우팅 시 outlet이 들어갈 부분*/} 
     </div>
   </div>
   )
