c5a2b21ff22b683261bdb13a8750d0b283515304
hanheel <hanhee630630@gmail.com>
2024-06-22 20:41:16 +0900

    feature : 프로필 컴포넌트

---
+++ src/Components/Profile/Profile_Info/Profile_Photo/ProfilePhoto.jsx
@@ -0,0 +1,9 @@
+import styles from './ProfilePhoto.module.css'
+
+const ProfilePhoto = ()=>{
+    return(
+        <div className={styles.myProfile__photo}>프로필 사진</div>
+    )
+}
+
+export default ProfilePhoto
