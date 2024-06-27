96ab8a1e45ad517a94b8949d1c682da134bf1079
yezixx <skylow917@daum.net>
2024-06-27 10:26:19 +0900

    Design; type props로 받도록 설정 - 디폴트: 테두리 및 배경색 변경 - ONLYBORDER: 테두리만 변경

--- src/Components/MemberItem/MemberItem.jsx
+++ src/Components/MemberItem/MemberItem.jsx
@@ -1,10 +1,14 @@
 import styles from "./MemberItem.module.css";
 
-const MemberItem = ({ memberName }) => {
+const MemberItem = ({ memberName, type }) => {
   return (
     <div className={styles.memberItem}>
       <div className={styles.memberItem__container}>
-        <button className={styles.memberItem__button}>
+        <button
+          className={`${styles.memberItem__button} ${
+            styles[`memberItem__button--${type}`]
+          }`}
+        >
           <div className={styles.memberItem__profilePic}>사진</div>
           <div className={styles.memberItem__name}>{memberName}</div>
         </button>
