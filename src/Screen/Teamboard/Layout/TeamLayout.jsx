9accddfbfd0bced84781d46a1fc872e0d04a6edf
yezixx <skylow917@daum.net>
2024-06-28 23:36:24 +0900

    Refactor: 상단 라벨 레이아웃 컴포넌트로 옮김

--- src/Screen/Teamboard/Layout/TeamLayout.jsx
+++ src/Screen/Teamboard/Layout/TeamLayout.jsx
@@ -5,8 +5,11 @@ import TeamContainer from "./Container/TeamContainer";
 const TeamLayout = () => {
   return (
     <div className={styles.teamLayout}>
-      <TeamNav />
-      <TeamContainer />
+      <div className={styles.teamLayout__label}>팀 게시판</div>
+      <div className={styles.teamLayout__container}>
+        <TeamNav />
+        <TeamContainer />
+      </div>
     </div>
   );
 };
--- src/Screen/Teamboard/Teamboard.jsx
+++ src/Screen/Teamboard/Teamboard.jsx
@@ -1,11 +1,10 @@
 import styles from "./Teamboard.module.css";
-import TeamLayout from "./Layout/TeamLayout";
+import { Outlet } from "react-router-dom";
 
 const Teamboard = () => {
   return (
     <div className={styles.teamboard}>
-      <div className={styles.teamboard__label}>팀 게시판</div>
-      <TeamLayout />
+      <Outlet />
     </div>
   );
 };
