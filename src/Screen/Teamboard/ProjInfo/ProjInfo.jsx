416f2083a0fac981fe7bbcdc240cf45b1eeb6d72
yezixx <skylow917@daum.net>
2024-07-04 22:54:26 +0900

    Design: 전체적인 요소 사이즈 및 배치 수정 - 캘린더 사이즈 조정 - 우측 팀원 목록, 버튼들 위치 조정

--- src/Screen/Teamboard/ProjInfo/ProjInfo.jsx
+++ src/Screen/Teamboard/ProjInfo/ProjInfo.jsx
@@ -18,27 +18,29 @@ const ProjInfo = () => {
   };
 
   return (
-    <div className={styles.projInfo}>
-      <div className={styles.calendar}>
-        <TeamCalendar />
-      </div>
-      <div className={styles.rightContent}>
-        <div className={styles.rightContent__memberList}>
-          <MemberList />
+    <div className={styles.projInfo__wrapper}>
+      <div className={styles.projInfo__container}>
+        <div className={styles.calendar}>
+          <TeamCalendar />
         </div>
-        <div className={styles.rightContent__buttonContainer}>
-          <div className={styles.rightContent__toolButton}>
-            {TOOLLIST.map((tool) => (
-              <CollabTool key={tool.id} tool={tool.tool} />
-            ))}
+        <div className={styles.rightContent}>
+          <div className={styles.rightContent__memberList}>
+            <MemberList />
           </div>
-          <div className={styles.rightContent__rightButtons}>
-            <Button text={"포트폴리오"} type={"SC_120x40"} />
-            <Button
-              text={"회의 인증"}
-              type={"MC2_120x40"}
-              onClick={navigateToManage}
-            />
+          <div className={styles.rightContent__buttonContainer}>
+            <div className={styles.rightContent__toolButton}>
+              {TOOLLIST.map((tool) => (
+                <CollabTool key={tool.id} tool={tool.tool} />
+              ))}
+            </div>
+            <div className={styles.rightContent__rightButtons}>
+              <Button text={"포트폴리오"} type={"SC_120x40"} />
+              <Button
+                text={"회의 인증"}
+                type={"MC2_120x40"}
+                onClick={navigateToManage}
+              />
+            </div>
           </div>
         </div>
       </div>
