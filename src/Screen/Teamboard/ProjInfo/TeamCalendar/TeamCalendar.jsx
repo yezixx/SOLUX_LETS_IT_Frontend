a9c12453bcbcb9a418310a86eb5e7ee7bd51ec90
yezixx <skylow917@daum.net>
2024-07-06 13:01:44 +0900

    Feat: 일정 추가 UI 구현

--- src/Screen/Teamboard/ProjInfo/TeamCalendar/TeamCalendar.jsx
+++ src/Screen/Teamboard/ProjInfo/TeamCalendar/TeamCalendar.jsx
@@ -1,21 +1,46 @@
 import "./TeamCalendar.css";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import FullCalendar from "@fullcalendar/react";
+import interactionPlugin from "@fullcalendar/interaction";
+import { useState } from "react";
+import ScheduleDialog from "./ScheduleDialog/ScheduleDialog";
 
+const mock_events = [
+  { title: "event 1", date: "2024-07-12" },
+  { title: "event 2", date: "2024-07-15" },
+  {
+    title: "event 3",
+    start: "2024-07-01",
+    end: "2024-07-03",
+  },
+];
 const TeamCalendar = () => {
-  const events = [
-    { title: "event 1", date: "2024-06-12" },
-    { title: "event 2", date: "2024-06-15" },
-  ];
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openDialog = () => {
+    setIsOpen(true);
+  };
+
+  const closeDialog = () => {
+    setIsOpen(false);
+  };
+
+  const onDateClick = () => {
+    console.log("달력 클릭");
+    openDialog();
+  };
 
   return (
     <div className="calendar">
+      {isOpen && <ScheduleDialog closeDialog={closeDialog} />}
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
-        events={events}
+        events={mock_events}
         timeZone="local"
         locale="kr"
+        selectable="true"
+        dateClick={onDateClick}
       />
     </div>
   );
