3f4322a6d10562ec1e2b51e29c592c5e39e39e88
yezixx <skylow917@daum.net>
2024-07-10 20:09:25 +0900

    Feat: 아이콘 선택 컴포넌트 렌더링

--- src/Components/CollabLink/CollabLink.jsx
+++ src/Components/CollabLink/CollabLink.jsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 import styles from "./CollabLink.module.css";
 import ToolIcon from "../ToolIcon/ToolIcon";
+import SelectIcon from "../SelectIcon/SelectIcon";
 
-const CollabLink = ({ link }) => {
+const CollabLink = ({ link, type }) => {
   const [url, setUrl] = useState(link);
+  const [visible, setVisible] = useState(false);
 
   const onChangeUrl = (e) => {
     setUrl(e.target.value);
   };
   return (
     <div className={styles.collabLink}>
-      <ToolIcon alt="아이콘" type="NONE" />
+      {visible === true ? (
+        <div className={styles.collabLink__selectIcon}>
+          <SelectIcon />
+        </div>
+      ) : null}
+      <div
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        <ToolIcon alt="아이콘" type={type === "SHORT" ? "" : "NONE"} />
+      </div>
       <input
-        className={styles.collabLink__input}
+        className={`${styles.collabLink__input} ${
+          styles[`collabLink__input--${type}`]
+        }`}
         type="text"
         value={url}
         onChange={onChangeUrl}
