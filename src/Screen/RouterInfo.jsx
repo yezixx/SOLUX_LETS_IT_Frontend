aba4b61aec8b30c37f1f692e50570226b34173c8
yezixx <skylow917@daum.net>
2024-07-13 00:04:35 +0900

    Feat: 라우팅 url 구조 작성

--- src/Screen/RouterInfo.jsx
+++ src/Screen/RouterInfo.jsx
@@ -8,11 +8,99 @@ import SetMember from "./Teamboard/SetMember/SetMember";
 import Teamboard from "./Teamboard/Teamboard";
 import CreateBoard from "./Teamboard/CreateBoard/CreateBoard";
 import CreateProfile from "./CreateProfile/CreateProfile";
+import MainHome from "./MainHome/MainHome";
+import SearchProject from "./SearchProject/SearchProject";
+import Proj_area from "./SearchProject/Proj_area";
+import ApplyHireProj from "./MyProject/ApplyHireProj/ApplyHireProj";
+import MyProfile from "./MyPage/MyProfile/MyProfile";
+import Proj_field from "./SearchProject/Proj_field";
+import Proj_fit from "./SearchProject/Proj_fit";
+import AttendProj from "./MyProject/AttendProj/AttendProj";
+import Scrap from "./MyProject/Scrap/Scrap";
+import MyPortfolio from "./MyPage/MyPortfolio/MyPortfolio";
+import PortfolioWrite from "./MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite";
+import CompletePort from "./MyPage/MyPortfolio/PortfolioWrite/Complete/CompletePort";
+import PortfolioBoard from "./MyPage/MyPortfolio/PortfolioBoard/PortfolioBoard";
 
 export const RouterInfo = [
+  //main 화면
   {
     path: "/",
+    element: <MainHome />,
+    index: true,
+  },
+
+  //프로젝트 찾기
+  {
+    path: "projects/home",
+    element: <SearchProject />,
+    label: "프로젝트 찾기 (전체)",
+  },
+  {
+    path: "projects/area",
+    element: <Proj_area />,
+    label: "지역별 찾기",
+  },
+  {
+    path: "projects/field",
+    element: <Proj_field />,
+    label: "분야별 찾기",
+  },
+  {
+    path: "projects/fit",
+    element: <Proj_fit />,
+    label: "맞춤 찾기",
+  },
+
+  //내 프로젝트
+  {
+    path: "myproj/applyhireproject",
+    element: <ApplyHireProj />,
+    label: "구인/신청 프로젝트",
+  },
+  {
+    path: "myproj/attendproj",
+    element: <AttendProj />,
+    label: "참여 프로젝트",
+  },
+  {
+    path: "myproj/scrap",
+    element: <Scrap />,
+    label: "스크랩 프로젝트",
+  },
+
+  //마이페이지
+  {
+    path: "mypage/profile",
+    element: <MyProfile />,
+    label: "프로필 관리",
+  },
+  {
+    path: "mypage/portfolio",
+    element: <MyPortfolio />,
+    label: "포트폴리오 관리",
+  },
+  {
+    path: "mypage/portfolio/post",
+    element: <PortfolioWrite />,
+    label: "포트폴리오 작성창",
+  },
+  {
+    path: "mypage/portfolio/post/summaryAI",
+    element: <CompletePort />,
+    label: "AI 생성 - 완성된 포트폴리오",
+  },
+  {
+    path: "mypage/portfolio/board",
+    element: <PortfolioBoard />,
+    label: "포트폴리오 게시판",
+  },
+
+  //팀 게시판
+  {
+    path: "teamboard",
     element: <Teamboard />,
+    label: "팀게시판",
     children: [
       {
         element: <TeamLayout />,
@@ -20,45 +108,45 @@ export const RouterInfo = [
           {
             index: true,
             element: <ProjInfo />,
-            label: "Project Information",
+            label: "프로젝트 정보",
           },
           {
             path: "member",
             element: <SetMember />,
-            label: "Set Member",
+            label: "팀원 설정r",
           },
           {
             path: "manage",
             element: <ManageProj />,
-            label: "Manage Project",
+            label: "프로젝트 관리",
           },
           {
             path: "manage/edit",
             element: <UpdateProj />,
-            label: "Update Project",
+            label: "프로젝트 정보 수정",
           },
           {
             path: "member/profile",
             element: <MemberProfile />,
-            label: "Member Profile",
+            label: "팀원 프로필",
           },
         ],
       },
       {
         path: "feedback",
         element: <TeamFeedback />,
-        label: "Team Feedback",
+        label: "팀원 평가",
+      },
+      {
+        path: "new",
+        element: <CreateBoard />,
+        label: "팀게시판 생성",
       },
     ],
   },
   {
-    path: "myproj/apply/createboard",
-    element: <CreateBoard />,
-    label: "Create Board",
-  },
-  {
-    path: "new/profile",
+    path: "profile/new",
     element: <CreateProfile />,
-    label: "Create Profile",
+    label: "최초 1회 프로필 생성",
   },
 ];
