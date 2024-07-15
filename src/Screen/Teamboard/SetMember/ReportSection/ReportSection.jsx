51d08705383d3b23dccd4b49859201dddc5e8b90
yezixx <skylow917@daum.net>
2024-07-15 21:19:02 +0900

    Design: tooltip onMouseEnter, onMouseLeave에 따라 visible 설정

--- src/Screen/Teamboard/SetMember/ReportSection/ReportSection.jsx
+++ src/Screen/Teamboard/SetMember/ReportSection/ReportSection.jsx
@@ -3,39 +3,47 @@ import styles from "./ReportSection.module.css";
 import QuestionMarkIcon from "../../../../Image/Icons/QuestionMarkIcon";
 import ToolTip from "../../../../Components/ToolTip/ToolTip";
 import Button from "../../../../Components/Button/Button";
+import { useState } from "react";
 
+const members = [
+  { id: 1, name: "유밍 BE" },
+  { id: 2, name: "도라" },
+  { id: 3, name: "Tom BE" },
+];
+
+const OPTIONS = [
+  { id: null, value: "강퇴 사유를 선택해주세요." },
+  { id: 0, value: "사유1" },
+  { id: 1, value: "사유2" },
+  { id: 2, value: "사유3" },
+  { id: 3, value: "사유4" },
+  { id: 4, value: "사유5" },
+  { id: 5, value: "사유6" },
+];
+
+const msg = (
+  <ul style={{ listStyleType: "disc", paddingLeft: "40px" }}>
+    <li>강퇴 : 프로그램 강제 퇴장 및 페널티</li>
+    <li>신고 : 프로그램은 그대로 진행 및 페널티</li>
+  </ul>
+);
 const ReportSection = () => {
-  const members = [
-    { id: 1, name: "유밍 BE" },
-    { id: 2, name: "도라" },
-    { id: 3, name: "Tom BE" },
-  ];
-
-  const OPTIONS = [
-    { id: null, value: "강퇴 사유를 선택해주세요." },
-    { id: 0, value: "사유1" },
-    { id: 1, value: "사유2" },
-    { id: 2, value: "사유3" },
-    { id: 3, value: "사유4" },
-    { id: 4, value: "사유5" },
-    { id: 5, value: "사유6" },
-  ];
-
-  const msg = (
-    <ul style={{ listStyleType: "disc", paddingLeft: "40px" }}>
-      <li>강퇴 : 프로그램 강제 퇴장 및 페널티</li>
-      <li>신고 : 프로그램은 그대로 진행 및 페널티</li>
-    </ul>
-  );
+  const [visible, setVisible] = useState(false);
 
   return (
     <div className={styles.reportSection}>
       <div className={styles.reportSection__label}>
         <div>강퇴 및 신고</div>
-        <div className={styles.reportSection__toolTip}>
-          <ToolTip msg={msg} width={"300px"} height={"60px"} />
-        </div>
-        <div className={styles.reportSection__icon}>
+        {visible && (
+          <div className={styles.reportSection__toolTip}>
+            <ToolTip msg={msg} width={"300px"} height={"60px"} />
+          </div>
+        )}
+        <div
+          className={styles.reportSection__icon}
+          onMouseEnter={() => setVisible(true)}
+          onMouseLeave={() => setVisible(false)}
+        >
           <QuestionMarkIcon width={"15px"} height={"15px"} />
         </div>
       </div>
