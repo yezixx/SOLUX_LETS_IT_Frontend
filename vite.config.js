e97fd48d1d0deea476842f52c5821383d4f507d1
yezixx <skylow917@daum.net>
2024-07-15 18:57:30 +0900

    Feat: proxy 설정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-})
-
-//아이콘을 컴포넌트처럼 쓰기 위함
+  server: {
+    proxy: "http://localhost:8080",
+  },
+});
