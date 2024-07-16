78b6796d6204e3f1279cce4779803a9b09cc4c69
hanheel <hanhee630630@gmail.com>
2024-07-16 17:15:48 +0900

    Feat : form  data 수집 후 post하는 코드 작성

--- src/Screen/MyPage/MyPortfolio/PortfolioWrite/FormInput.jsx
+++ src/Screen/MyPage/MyPortfolio/PortfolioWrite/FormInput.jsx
@@ -1,15 +1,18 @@
 const FormInput = ({
+  name = undefined,
   width = "100%",
   height = "20px",
   border = "1px solid var(--line-color)",
   placeholder = "",
   type = "",
   textarea = false,
+  onChange = null,
 }) => {
   return (
     //textarea 일 경우
     textarea ? (
       <textarea
+        name={name}
         style={{
           width: width,
           height: height,
@@ -19,10 +22,12 @@ const FormInput = ({
           padding: "10px",
           resize: "none",
         }}
+        onChange={onChange}
       ></textarea>
     ) : (
       //textarea외 input 태그일 경우
       <input
+        name={name}
         type={type}
         placeholder={placeholder}
         style={{
@@ -33,6 +38,7 @@ const FormInput = ({
           borderRadius: "4px",
           padding: "10px",
         }}
+        onChange={onChange}
       ></input>
     )
   );
--- src/Screen/MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite.jsx
+++ src/Screen/MyPage/MyPortfolio/PortfolioWrite/PortfolioWrite.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
 import Button from "../../../../Components/Button/Button";
 import SearchIcon from "../../../../Image/Icons/SearchIcon";
 import FormInput from "./FormInput";
 import styles from "./PortfolioWrite.module.css";
 
 const PortfolioWrite = () => {
+  const [portfolioData, setPortfolioData] = useState({
+    title: "",
+    role: "",
+    stack: "",
+    tasks: "",
+    difficulties: "",
+    solutions: "",
+    learnings: "",
+  });
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setPortfolioData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  //백엔드 전송 함수
+  // const handleSubmit = (e) => {  //이 함수는 form에 onsubmit으로 붙임
+  //   e.preventDefault();
+  //   // 데이터 백엔드 전송 함수 호출
+  // 예시
+  /*    // Axios를 사용하여 서버로 데이터 전송
+    axios.post('/api/portfolio', portfolioData)
+      .then(response => {
+        console.log("데이터 전송 성공:", response);
+        // 서버로부터의 응답 처리
+      })
+      .catch(error => {
+        console.error("데이터 전송 실패:", error);
+        // 오류 처리
+      });
+  }; */
+  //   submitFormData(formData);
+  // };
+
+  console.log(portfolioData);
   return (
     <div className={styles.portfolioWrite__contentWrap}>
       {/*프로젝트명 + 포트폴리오 작성 폼 */}
@@ -14,39 +51,71 @@ const PortfolioWrite = () => {
       {/*포트폴리오 작성 폼 */}
       <form className={styles.portfolioWrite__form}>
         <input
+          name="title"
+          onChange={onChange}
           className={styles.portfolioWrite__form__title}
           placeholder="제목을 입력하세요"
         ></input>
 
         <label htmlFor="role">
           담당역할
-          <FormInput width="60%" placeholder="백엔드" />
+          <FormInput
+            name="role"
+            onChange={onChange}
+            width="60%"
+            placeholder="백엔드"
+          />
         </label>
 
         <label htmlFor="stack">
           사용스택
-          <FormInput width="60%" placeholder="Spring" />
+          <FormInput
+            name="stack"
+            onChange={onChange}
+            width="60%"
+            placeholder="Spring"
+          />
           <SearchIcon bgc="var(--text-color2)" />
         </label>
 
         <div className={styles.portfolioWrite__form__tasks}>
           <label htmlFor="tasks">이번 주 한 일</label>
-          <FormInput textarea={true} type="textarea" height="90px" />
+          <FormInput
+            name="tasks"
+            onChange={onChange}
+            textarea={true}
+            height="90px"
+          />
         </div>
 
         <div>
-          <label htmlFor="difficult">발생한 문제 / 어려움</label>
-          <FormInput textarea={true} height="200px" />
+          <label htmlFor="difficulties">발생한 문제 / 어려움</label>
+          <FormInput
+            name="difficulties"
+            onChange={onChange}
+            textarea={true}
+            height="200px"
+          />
         </div>
 
         <div>
           <label htmlFor="result">해결 방법</label>
-          <FormInput textarea={true} height="200px" />
+          <FormInput
+            name="solutions"
+            onChange={onChange}
+            textarea={true}
+            height="200px"
+          />
         </div>
 
         <div className={styles.portfolioWrite__form__thoughts}>
           <label htmlFor="thoughts">새로 알게된 점 / 깨달은 점</label>
-          <FormInput textarea={true} width="100%" height="195px" />
+          <FormInput
+            name="learnings"
+            onChange={onChange}
+            textarea={true}
+            height="195px"
+          />
         </div>
 
         {/*폼 제출 버튼*/}
