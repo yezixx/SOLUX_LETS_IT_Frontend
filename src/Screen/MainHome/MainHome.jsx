f5982255e8b803bdec42b8a6952ae5e6726a760c
jimin <rr44wlals@gmail.com>
2024-07-16 03:15:38 -0700

    mainhome commit 1

--- src/Screen/MainHome/MainHome.jsx
+++ src/Screen/MainHome/MainHome.jsx
@@ -5,18 +5,27 @@ import MainManageButton from "../../Components/MainManegeButton/MainManageButton
 import RecommendProject from "../../Components/RecommendProject/RecommendProject";
 import PopularProject from "../../Components/PopularProject/PopularProject";
 
+import Contest from "../../Components/Contest/Contest";
+import News from "../../Components/News/News";
+
 function MainHome() {
   return (
-    <div className={styles.wrap}>
-      <MainTitle />
-      <MainManageButton />
-      <RecommendProject />
-      <PopularProject />
-      <div className={styles.main}>
-        {/*<MyProfile />*/} {/*라우팅 시 outlet이 들어갈 부분*/}
-      </div>
+  <div className={styles.wrap}>
+    
+    <MainTitle />
+    <MainManageButton/>
+    <RecommendProject />
+    <PopularProject />
+    <div className={styles.contestnews}>
+      <Contest/>
+      <News/>
+
     </div>
-  );
+    <div className={styles.main}> 
+    {/*<MyProfile />*/} {/*라우팅 시 outlet이 들어갈 부분*/}
+    </div>
+  </div>
+  )
 }
 
 export default MainHome;
