7eb6bb4256ab502a789070a189e1a57cfa804fda
yezixx <skylow917@daum.net>
2024-07-17 21:36:26 +0900

    Feat: 불참인원 체크 기능 - 참여인원, 불참인원 카운트 - 불참 인원 팀데이터로 넘겨서 추가해줌 - 첨부파일 기능만 남음

--- src/Screen/Teamboard/ManageProj/AttendanceDetail/AttendanceDetail.jsx
+++ src/Screen/Teamboard/ManageProj/AttendanceDetail/AttendanceDetail.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Button from "../../../../Components/Button/Button";
 import MemberItem from "../../../../Components/MemberItem/MemberItem";
 import { TeamDispatchContext, TeamStateContext } from "../../Teamboard";
@@ -9,13 +9,30 @@ const AttendanceDetail = () => {
   const members = teamData.members;
   const meetings = teamData.meetingLog;
   const { onSaveMeeting } = useContext(TeamDispatchContext);
+  const [selectedMember, setSelectedMember] = useState();
+  const [proofImage, setProofImage] = useState();
 
   const onClickSave = () => {
+    if (selectedMember && members.length - selectedMember.length < 1) {
+      alert("참여인원은 1명 이상이어야 합니다.");
+      return;
+    }
     onSaveMeeting({
       date: new Date().toISOString().split("T")[0],
-      nonParticipants: ["tom"],
+      nonParticipants: selectedMember ? selectedMember : [],
       proofImages: "third_week.png",
     });
+    setSelectedMember(null);
+  };
+
+  const onClickMember = (userId) => {
+    if (selectedMember && selectedMember.includes(userId)) {
+      setSelectedMember(
+        selectedMember.filter((member) => String(member) !== String(userId))
+      );
+    } else if (selectedMember && !selectedMember.includes(userId))
+      setSelectedMember([...selectedMember, userId]);
+    else setSelectedMember([userId]);
   };
 
   return (
@@ -27,10 +44,14 @@ const AttendanceDetail = () => {
         <div className={styles.attendanceDetail__file}>
           <div className={styles.attendanceDetail__fileLabel}>
             <div className={styles.attendanceDetail__fileName}>
-              this_week.png
+              {proofImage}
             </div>
             <div className={styles.attendanceDetail__countLabel}>
-              참여인원 3 | 불참인원 1
+              참여인원{" "}
+              {selectedMember
+                ? members.length - selectedMember.length
+                : members.length}{" "}
+              | 불참인원 {selectedMember ? selectedMember.length : 0}
             </div>
           </div>
           <Button text="첨부파일" type="SEC_80x30" />
@@ -38,9 +59,26 @@ const AttendanceDetail = () => {
         <div className={styles.attendanceDetail__list}>
           <div className={styles.attendanceDetail__listLabel}>불참한 팀원</div>
           <div className={styles.attendanceDetail__members}>
-            {members.map((member) => (
-              <MemberItem key={member.id} memberName={member.name} />
-            ))}
+            {members.map((member) =>
+              selectedMember && selectedMember.includes(member.userId) ? (
+                <MemberItem
+                  key={member.id}
+                  memberName={member.name}
+                  type="SELECTED"
+                  onClick={() => {
+                    onClickMember(member.userId);
+                  }}
+                />
+              ) : (
+                <MemberItem
+                  key={member.id}
+                  memberName={member.name}
+                  onClick={() => {
+                    onClickMember(member.userId);
+                  }}
+                />
+              )
+            )}
           </div>
         </div>
         <div className={styles.attendanceDetail__button}>
