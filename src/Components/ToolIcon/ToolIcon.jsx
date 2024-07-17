1291185c1ffc269bed0c1f7116e942e147680027
yezixx <skylow917@daum.net>
2024-07-17 19:41:16 +0900

    Feat: img태그 title 추가

--- src/Components/ToolIcon/ToolIcon.jsx
+++ src/Components/ToolIcon/ToolIcon.jsx
@@ -1,12 +1,12 @@
 import styles from "./ToolIcon.module.css";
 
-const ToolIcon = ({ src, alt, type, onClick }) => {
+const ToolIcon = ({ src, alt, type, title, onClick }) => {
   return (
     <button
       className={`${styles.toolIcon} ${styles[`toolIcon--${type}`]}`}
       onClick={onClick}
     >
-      <img src={src} alt={alt} />
+      <img src={src} alt={alt} title={title} />
     </button>
   );
 };
