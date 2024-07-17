0cea96a53f8d777b02de636a5ce5a9c4ffe35748
hanheel <hanhee630630@gmail.com>
2024-07-17 13:58:23 +0900

    Fix : 프록시 설정 - 백엔드 주소 /api 로 받아올 수 있게끔 수정

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,22 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
+//vite 프록시 설정
+//서버 주소 full x, /api 로 불러올 수 있음
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
-    proxy: "http://localhost:8080",
+    proxy: {
+      //프론트엔드 상에서 대치할 주소명
+      "/api": {
+        //백엔드 서버 주소
+        target: "http://172.20.8.20:8080",
+        // 프론트엔드 상에서 다른 주소 (api)로 대치할 것인가? true
+        changeOrigin: true,
+        // /api로 보낸 요청 api를 제거하고 실제 요청 주소로 대치
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
+//vite 설정, 프록시 설정
