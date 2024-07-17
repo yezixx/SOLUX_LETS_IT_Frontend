3f7f106b117b46da6d7ac35165dfd1e295d64582
yezixx <skylow917@daum.net>
2024-07-17 22:42:09 +0900

    Feat: 이미지 파일 첨부 기능 구현

--- src/Screen/Teamboard/ManageProj/AttendanceDetail/AttendanceDetail.jsx
+++ src/Screen/Teamboard/ManageProj/AttendanceDetail/AttendanceDetail.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import Button from "../../../../Components/Button/Button";
 import MemberItem from "../../../../Components/MemberItem/MemberItem";
 import { TeamDispatchContext, TeamStateContext } from "../../Teamboard";
@@ -11,18 +11,26 @@ const AttendanceDetail = () => {
   const { onSaveMeeting } = useContext(TeamDispatchContext);
   const [selectedMember, setSelectedMember] = useState();
   const [proofImage, setProofImage] = useState();
+  const [placeholder, setPlaceholder] = useState("파일을 선택해주세요.");
+  const imgRef = useRef();
 
   const onClickSave = () => {
     if (selectedMember && members.length - selectedMember.length < 1) {
       alert("참여인원은 1명 이상이어야 합니다.");
       return;
     }
+    if (!proofImage) {
+      alert("회의 인증 사진을 첨부해주세요.");
+      return;
+    }
     onSaveMeeting({
       date: new Date().toISOString().split("T")[0],
       nonParticipants: selectedMember ? selectedMember : [],
-      proofImages: "third_week.png",
+      proofImages: proofImage,
     });
     setSelectedMember(null);
+    setPlaceholder("파일을 선택해주세요.");
+    setProofImage(null);
   };
 
   const onClickMember = (userId) => {
@@ -35,6 +43,12 @@ const AttendanceDetail = () => {
     else setSelectedMember([userId]);
   };
 
+  const onChangeFile = () => {
+    const fileName = imgRef.current.files[0].name;
+    setProofImage(/*imgRef.current.files*/ fileName);
+    setPlaceholder(fileName);
+  };
+
   return (
     <div className={styles.attendanceDetail}>
       <div className={styles.attendanceDetail__container}>
@@ -43,9 +57,11 @@ const AttendanceDetail = () => {
         </div>
         <div className={styles.attendanceDetail__file}>
           <div className={styles.attendanceDetail__fileLabel}>
-            <div className={styles.attendanceDetail__fileName}>
-              {proofImage}
-            </div>
+            <input
+              placeholder={placeholder}
+              disabled
+              className={styles.attendanceDetail__fileName}
+            />
             <div className={styles.attendanceDetail__countLabel}>
               참여인원{" "}
               {selectedMember
@@ -54,7 +70,14 @@ const AttendanceDetail = () => {
               | 불참인원 {selectedMember ? selectedMember.length : 0}
             </div>
           </div>
-          <Button text="첨부파일" type="SEC_80x30" />
+          <label htmlFor="input_img">첨부파일</label>
+          <input
+            type="file"
+            id="input_img"
+            ref={imgRef}
+            onChange={onChangeFile}
+            accept="image/*"
+          />
         </div>
         <div className={styles.attendanceDetail__list}>
           <div className={styles.attendanceDetail__listLabel}>불참한 팀원</div>
