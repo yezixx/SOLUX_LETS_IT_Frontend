b57400a45cfbea9ffd0cd69391c7813bc0fe520d
hanheel <hanhee630630@gmail.com>
2024-07-19 17:02:46 +0900

    Feat : Apply 백엔드 연동

--- src/Screen/MyProject/ApplyHireProj/ApplicantBtn/ApplicantBtn.jsx
+++ src/Screen/MyProject/ApplyHireProj/ApplicantBtn/ApplicantBtn.jsx
@@ -1,14 +1,13 @@
-import { useAtom, useAtomValue } from "jotai";
 import Button from "../../../../Components/Button/Button";
 import ProfilePhoto from "../../../../Components/Profile/Profile_Info/Profile_Photo/ProfilePhoto";
 import styles from "./ApplicantBtn.module.css";
 import { getApplicantList } from "../../../../service/applyService";
 import { applicantListAtom } from "../../../../atoms/atoms";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const ApplicantBtn = ({ postId }) => {
-  /*프로젝트 리스트를 담아올 atom 설정정 */
-  const [applicantList, setApplicantList] = useAtom(applicantListAtom);
+  /*구인/신청 프로젝트 리스트를 담아올 atom 설정 */
+  const [applicantList, setApplicantList] = useState(applicantListAtom);
 
   useEffect(() => {
     getApplicantList({ postId }) // 가져오는 프로젝트 data에 따라 매개변수를 바꿔야 함
