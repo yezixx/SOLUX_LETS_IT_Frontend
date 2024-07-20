fd1132ff38ea80022a6e9c236a5fa54ad6b4856c
yezixx <skylow917@daum.net>
2024-07-20 16:30:18 +0900

    Fix: 에러 발생하는 부분 주석처리

--- src/Screen/MyProject/ApplyHireProj/ApplicantBtn/ApplicantBtn.jsx
+++ src/Screen/MyProject/ApplyHireProj/ApplicantBtn/ApplicantBtn.jsx
@@ -2,12 +2,12 @@ import Button from "../../../../Components/Button/Button";
 import ProfilePhoto from "../../../../Components/Profile/Profile_Info/Profile_Photo/ProfilePhoto";
 import styles from "./ApplicantBtn.module.css";
 import { getApplicantList } from "../../../../service/applyService";
-import { applicantListAtom } from "../../../../atoms/atoms";
+//import { applicantListAtom } from "../../../../atoms/atoms";
 import { useEffect, useState } from "react";
 
 const ApplicantBtn = ({ postId }) => {
   /*구인/신청 프로젝트 리스트를 담아올 atom 설정 */
-  const [applicantList, setApplicantList] = useState(applicantListAtom);
+  const [applicantList, setApplicantList] = useState(/*applicantListAtom*/);
 
   useEffect(() => {
     getApplicantList({ postId }) // 가져오는 프로젝트 data에 따라 매개변수를 바꿔야 함
