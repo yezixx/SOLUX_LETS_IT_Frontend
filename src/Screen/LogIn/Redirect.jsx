76d52ff024c10ea43b8b506b46092a699ce98a3b
hanheel <hanhee630630@gmail.com>
2024-07-22 23:07:04 +0900

    Fix : 리다이렉트 성공

--- src/Screen/LogIn/Redirect.jsx
+++ src/Screen/LogIn/Redirect.jsx
@@ -1,8 +1,8 @@
-import axios from "axios";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
 
-const Redirect = (props) => {
+const Redirect = () => {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
   console.log(code);
@@ -14,20 +14,31 @@ const Redirect = (props) => {
         url: `${import.meta.env.VITE_REDIRECT_URL}/?code=${code}`,
         headers: {
           "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
-          "Access-Control-Allow-Origin": "*", //cors 에러 방지
         },
       }).then((res) => {
-        //백에서 완료후 우리사이트 전용 토큰 넘겨받음
-        console.log(res);
-        //사용할 정보 localstorage에 저장
-        localStorage.setItem("name", res.data.account.kakaoName);
+        //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
+        console.log(res.data);
+        const userData = res.data.user;
+        //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
+        localStorage.setItem("user", JSON.stringify(userData));
         //로그인이 성공하면 이동할 페이지
         navigate("/");
       });
     };
-    kakaoLogin();
-  }, [props.history]);
-  return <h1>로그인 중입니다.</h1>;
+    if (code) {
+      kakaoLogin();
+    }
+  }, [code, navigate]);
+
+  return (
+    <div className="LoginHandeler">
+      <div className="notice">
+        <p>로그인 중입니다.</p>
+        <p>잠시만 기다려주세요.</p>
+        <div className="spinner"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Redirect;
--- src/service/apiClient.js
+++ src/service/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://192.168.37.36:8080",
+  baseURL: "http://192.168.14.139:8080",
   headers: {
     "Content-Type": "application/json",
   },
