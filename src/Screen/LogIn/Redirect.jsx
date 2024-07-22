1c5f6c1d108daeb50995f3db14e3119c6e0318fc
hanheel <hanhee630630@gmail.com>
2024-07-22 23:56:18 +0900

    Feat: 로그아웃 기능 구현

--- src/Screen/LogIn/Redirect.jsx
+++ src/Screen/LogIn/Redirect.jsx
@@ -1,10 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
+import { useSetAtom } from "jotai";
+import { isLoginAtom } from "../../atoms/atoms";
 
 const Redirect = () => {
   const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
+  const setIsLogIn = useSetAtom(isLoginAtom);
   console.log(code);
   //인가코드 백으로 보내는 코드
   useEffect(() => {
@@ -16,11 +19,14 @@ const Redirect = () => {
           "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
         },
       }).then((res) => {
+        console.log(JSON.stringify(res, null, 1));
         //백에서 완료후 우리사이트 전용 토큰 넘겨주는게 성공했다면
-        console.log(res.data);
+        setIsLogIn(true);
         const userData = res.data.user;
         //계속 쓸 정보들( ex: 이름) 등은 localStorage에 저장해두자
         localStorage.setItem("user", JSON.stringify(userData));
+        //로그인 상태 true로 변경
+        localStorage.setItem("isLoggedIn", "true");
         //로그인이 성공하면 이동할 페이지
         navigate("/");
       });
--- src/atoms/atoms.js
+++ src/atoms/atoms.js
@@ -3,3 +3,5 @@ import { atom } from "jotai";
 export const userIdAtom = atom(1);
 //프로젝트 신청창 form
 export const applicant = atom([]);
+//사용자 로그인 여부
+export const isLoginAtom = atom(false);
