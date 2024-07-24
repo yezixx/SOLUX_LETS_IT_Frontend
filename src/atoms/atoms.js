34fa4805c9f538ac069b9aba6d4254e6b06a718d
hanheel <hanhee630630@gmail.com>
2024-07-25 03:01:22 +0900

    Feat: 프로젝트 구인구직 창 구현

--- src/atoms/atoms.js
+++ src/atoms/atoms.js
@@ -7,3 +7,15 @@ export const userAtom = atom({});
 export const applicant = atom([]);
 //사용자 로그인 여부
 export const isLoginAtom = atom(false);
+//구인글 작성 (세부화면으로 쪼갰기에 atom 필요)
+export const postProjectAtom = atom({
+  peopleNum: "",
+  endDate: "",
+  preference: "",
+  projectInfo: "",
+  regionId: "",
+  projectPeriod: "",
+  ageGroup: "",
+  stack: "",
+  content: "",
+});
