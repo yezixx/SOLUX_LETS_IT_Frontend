b013cfaac41f6d5b0466f23672ed71fa0c56fff1
yezixx <skylow917@daum.net>
2024-07-25 16:22:49 +0900

    Design: "."제거 텍스트 color 지정

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import Spinner from "../../assets/loadingSpin.gif";
 const Loading = () => {
   return (
     <div className={styles.Loading}>
-      <div className={styles.Loading__text}>잠시만 기다려 주세요.</div>
+      <div className={styles.Loading__text}>잠시만 기다려 주세요</div>
       <img src={Spinner} alt="loading" width="5%" />
     </div>
   );
