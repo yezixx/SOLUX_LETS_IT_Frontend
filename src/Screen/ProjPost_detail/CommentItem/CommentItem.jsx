3eb7fd24150faaaf6c343894bfc9ada0de4923f6
yezixx <skylow917@daum.net>
2024-07-25 23:12:15 +0900

    Fix: 날자 포맷 수정

--- src/Screen/ProjPost_detail/CommentItem/CommentItem.jsx
+++ src/Screen/ProjPost_detail/CommentItem/CommentItem.jsx
@@ -10,12 +10,15 @@ import { userIdAtom } from "../../../atoms/atoms";
 
 const getFormattedDate = (date) => {
   const dateObj = new Date(date);
-  return `${dateObj.getFullYear()}-${
-    dateObj.getMonth() + 1
-  }-${dateObj.getDate()}, ${dateObj.getHours()}:${dateObj
-    .getMinutes()
+  return `${dateObj.getFullYear()}-${(dateObj.getMonth() + 1)
     .toString()
-    .padStart(2, "0")}`;
+    .padStart(2, "0")}-${dateObj
+    .getDate()
+    .toString()
+    .padStart(2, "0")}, ${dateObj
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${dateObj.getMinutes().toString().padStart(2, "0")}`;
 };
 
 const CommentItem = ({
