d80369a56ea99dd2341730e442bbc5351e621511
yezixx <skylow917@daum.net>
2024-07-25 16:14:42 +0900

    Fix: 사진 파일이 유효하지 않을 때 기본 프로필 사진으로 대체

--- src/Components/MemberItem/MemberItem.jsx
+++ src/Components/MemberItem/MemberItem.jsx
@@ -1,7 +1,14 @@
 import styles from "./MemberItem.module.css";
 import defaultProfilePic from "../../assets/user.svg";
+import { useState } from "react";
 
 const MemberItem = ({ memberName, profilePic, type, onClick }) => {
+  const [imgSrc, setImgSrc] = useState(profilePic || defaultProfilePic);
+
+  const handleImgError = () => {
+    setImgSrc(defaultProfilePic);
+  };
+
   return (
     <div className={styles.memberItem}>
       <div className={styles.memberItem__container}>
@@ -14,7 +21,8 @@ const MemberItem = ({ memberName, profilePic, type, onClick }) => {
         >
           <img
             className={styles.memberItem__profilePic}
-            src={profilePic ? profilePic : defaultProfilePic}
+            src={imgSrc}
+            onError={handleImgError}
           />
           <div className={styles.memberItem__name}>{memberName}</div>
         </button>
