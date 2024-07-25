9bf3be2c20bc6da1eb66c4cb4d2a891c3f2f3d42
yezixx <skylow917@daum.net>
2024-07-25 18:02:26 +0900

    Test: userIdAtom에 test용 아이디 설정

--- src/atoms/atoms.js
+++ src/atoms/atoms.js
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 //삭제 예정
-export const userIdAtom = atom(1);
+export const userIdAtom = atom("letsit_backend.model.Member@72f9fde6");
 
 export const userAtom = atom({});
 //프로젝트 신청창 form
