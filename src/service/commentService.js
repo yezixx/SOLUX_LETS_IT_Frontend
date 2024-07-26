88bc73e40e72f20105e03bba7ed90512fdbd2ffe
yezixx <skylow917@daum.net>
2024-07-26 21:12:27 +0900

    Fix: comments api service 코드 수정

--- src/service/commentService.js
+++ src/service/commentService.js
@@ -1,19 +1,40 @@
 import apiClient from "./apiClient";
 
 // 댓글 등록 - 구인글 상세 보기 화면에서 댓글 작성 후 등록 버튼
-export const createComment = (postId, commentData) => {
-  return apiClient.post(`comments/${postId}/upload`, commentData);
+export const createComment = async (postId, commentData) => {
+  try {
+    const response = await apiClient.post(
+      `comments/${postId}/upload`,
+      commentData
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching create comment", error);
+    throw error;
+  }
 };
 
 // 댓글 수정 - 구인글 상세 보기 화면에서 댓글 수정 후 수정 버튼
-export const updateComment = (postId, commentId, updatedCommentData) => {
-  return apiClient.patch(
-    `comments/${postId}/update/${commentId}`,
-    updatedCommentData
-  );
+export const updateComment = async (postId, commentId, updatedCommentData) => {
+  try {
+    const response = await apiClient.patch(
+      `comments/${postId}/update/${commentId}`,
+      updatedCommentData
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching update comment", error);
+    throw error;
+  }
 };
 
 // 댓글 삭제 - 구인글 상세 보기 화면에서 댓글 삭제 버튼
-export const deleteComment = (commentId) => {
-  return apiClient.delete(`comments/${commentId}/delete`);
+export const deleteComment = async (commentId) => {
+  try {
+    const response = await apiClient.delete(`comments/${commentId}/delete`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching delete comment", error);
+    throw error;
+  }
 };
