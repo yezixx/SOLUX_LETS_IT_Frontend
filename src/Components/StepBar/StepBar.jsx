dc036e6ba70c05f86b42ae4e77053b47a84bcc46
yezixx <skylow917@daum.net>
2024-07-27 15:25:07 +0900

    Design: 스텝 안내 제거 및 문구 수정

--- src/Components/StepBar/StepBar.jsx
+++ src/Components/StepBar/StepBar.jsx
@@ -4,9 +4,10 @@ const StepBar = ({ type }) => {
   return (
     <div className={styles.stepBar}>
       <div className={styles.stepBar__label}>
-        프로젝트에 신청하기 위해선 최초 1회 <span>실명 인증</span> 및
+        프로젝트에 신청하기 위해선 최초 1회 {/*<span>실명 인증</span> 및*/}
         <span> 프로필 작성</span>이 필요합니다.
       </div>
+      {/*
       <div className={styles.stepBar__step}>
         <div className={styles.stepBar__stepNum}>
           <div className={styles["stepBar__num--COMPLETE"]}>1</div>
@@ -35,7 +36,7 @@ const StepBar = ({ type }) => {
             프로필 작성
           </div>
         </div>
-      </div>
+      </div>*/}
     </div>
   );
 };
