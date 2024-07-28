7c8b5868a06c425c90a2065b605494bea80a0442
hanheel <hanhee630630@gmail.com>
2024-07-28 16:22:34 +0900

    Feat: 포트폴리오 작성란 focus 기능 완료

--- src/Screen/MyPage/MyPortfolio/PortfolioWrite/FormInput.jsx
+++ src/Screen/MyPage/MyPortfolio/PortfolioWrite/FormInput.jsx
@@ -1,46 +1,24 @@
+import styles from "./FormInput.module.css";
+
 const FormInput = ({
   name = undefined,
   width = "100%",
   height = "20px",
-  border = "1px solid var(--line-color)",
-  placeholder = "",
-  type = "",
-  textarea = false,
   onChange = null,
+  error, //에러가 발생한 key 값
 }) => {
   return (
-    //textarea 일 경우
-    textarea ? (
-      <textarea
-        name={name}
-        style={{
-          width: width,
-          height: height,
-          border: border,
-          backgroundColor: "#fff",
-          borderRadius: "4px",
-          padding: "10px",
-          resize: "none",
-        }}
-        onChange={onChange}
-      ></textarea>
-    ) : (
-      //textarea외 input 태그일 경우
-      <input
-        name={name}
-        type={type}
-        placeholder={placeholder}
-        style={{
-          width: width,
-          height: height,
-          border: border,
-          backgroundColor: "#fff",
-          borderRadius: "4px",
-          padding: "10px",
-        }}
-        onChange={onChange}
-      ></input>
-    )
+    <textarea
+      className={`${styles.formInput__textarea} ${
+        error.includes(name) ? styles.formError : ""
+      }`}
+      name={name}
+      style={{
+        width: width,
+        height: height,
+      }}
+      onChange={onChange}
+    />
   );
 };
 
--- src/Screen/MyPage/MyPortfolio/PortfolioWrite/usePortPost.js
+++ src/Screen/MyPage/MyPortfolio/PortfolioWrite/usePortPost.js
@@ -1,6 +1,17 @@
-import { useState } from "react";
+import { startTransition, useState } from "react";
+import { postPortfolios } from "../../../../service/portfolioService";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAtomValue } from "jotai";
+import { userIdAtom } from "../../../../atoms/atoms";
+import { useValidate } from "../../../../Hooks/useValidate";
 
 const usePortPost = () => {
+  //화면 이동
+  const navigate = useNavigate();
+  const navigateTo = (link) => {
+    navigate(link);
+  };
+  //백엔드에 보낼 데이터 담을 state
   const [portfolioData, setPortfolioData] = useState({
     prtTitle: "",
     // role: "",
@@ -10,6 +21,7 @@ const usePortPost = () => {
     solutions: "",
     feedback: "",
   });
+  //백엔드 보낼 데이터 저장
   const onChange = (e) => {
     const { name, value } = e.target;
     setPortfolioData((prevData) => ({
@@ -17,6 +29,29 @@ const usePortPost = () => {
       [name]: value,
     }));
   };
-  return { portfolioData, onChange };
+  //유효성 검사
+  const { validation, ApplyFormError } = useValidate(portfolioData);
+
+  //teamId
+  const { teamId } = useParams();
+  //userId 변경 필요
+  const userId = useAtomValue(userIdAtom);
+  //백엔드 전송 함수
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validation()) {
+      const confirmSubmit = window.confirm("포트폴리오를 제출하시겠습니까?");
+      if (confirmSubmit) {
+        startTransition(() => {
+          postPortfolios(teamId, userId, portfolioData);
+        });
+        navigateTo(`/mypage/portfolio/board/${teamId}`); // 우선은 홈 화면으로 이동
+      } else {
+      }
+    } else {
+      alert("폼을 모두 작성해 주세요");
+    }
+  };
+  return { portfolioData, onChange, handleSubmit, navigateTo, ApplyFormError };
 };
 export default usePortPost;
