ad713b2466f9448220e7e25a4a4604a4934c9f8a
hanheel <hanhee630630@gmail.com>
2024-07-30 16:46:48 +0900

    Feat: 로그인 접근제한 설정정

--- src/Screen/RouterInfo.jsx
+++ src/Screen/RouterInfo.jsx
@@ -78,7 +78,7 @@ export const RouterInfo = [
       // 구인글 작성하기
       {
         path: "projects/post",
-        element: <ProjectHire />,
+        element: <PrivateRoute element={<ProjectHire />} />,
         label: "구인글 작성하기",
       },
       // 구인글 보기, 추후  /:id 추가 필요
@@ -91,12 +91,12 @@ export const RouterInfo = [
       //내 프로젝트
       {
         path: "myproj",
-        element: <MyProjectLayout />,
+        element: <PrivateRoute element={<MyProjectLayout />} />,
         label: "구인/신청 프로젝트",
         children: [
           {
             path: "hiring-and-applied",
-            element: <ApplyHireProj />,
+            element: <PrivateRoute element={<ApplyHireProj />} />,
             label: "구인/신청 프로젝트",
           },
           {
@@ -104,7 +104,7 @@ export const RouterInfo = [
             path: "applyprofile/:applyId",
             // path: "applyprofile",
             element: <PrivateRoute element={<ApplyProfile />} />,
-            label: "구인/신청 프로젝트",
+            label: "신청자 프로필",
           },
           {
             path: "attendproj",
@@ -121,40 +121,40 @@ export const RouterInfo = [
       //마이페이지
       {
         path: "mypage",
-        element: <MyPageLayout />,
+        element: <PrivateRoute element={<MyPageLayout />} />,
         label: "프로필 관리",
         children: [
           {
             path: "profile",
-            element: <MyProfile />,
+            element: <PrivateRoute element={<MyProfile />} />,
             label: "프로필 관리",
           },
           {
             // path: "portfolio/:prjId",
             path: "portfolio",
-            element: <MyPortfolio />,
+            element: <PrivateRoute element={<MyPortfolio />} />,
             label: "포트폴리오 관리",
           },
           {
             path: "portfolio/post/:teamId",
-            element: <PortfolioWrite />,
+            element: <PrivateRoute element={<PortfolioWrite />} />,
             label: "포트폴리오 작성창",
           },
           {
             path: "portfolio/post/summaryAI",
-            element: <CompletePort />,
+            element: <PrivateRoute element={<CompletePort />} />,
             label: "AI 생성 - 완성된 포트폴리오",
           },
           {
             path: "portfolio/board/:teamId",
             // path: "portfolio/board",
-            element: <PortfolioBoard />,
+            element: <PrivateRoute element={<PortfolioBoard />} />,
             label: "포트폴리오 게시판",
           },
           {
             path: "portfolio/board/:teamId/detail/:prtId",
             // path: "portfolio/board/detail",
-            element: <PortfolioDetail />,
+            element: <PrivateRoute element={<PortfolioDetail />} />,
             label: "포트폴리오 게시판",
           },
         ],
@@ -163,7 +163,7 @@ export const RouterInfo = [
       //팀 게시판
       {
         path: "teamboard",
-        element: <Teamboard />,
+        element: <PrivateRoute element={<Teamboard />} />,
         label: "팀게시판",
         children: [
           {
@@ -205,7 +205,7 @@ export const RouterInfo = [
       },
       {
         path: "teamboard/new",
-        element: <CreateBoard />,
+        element: <PrivateRoute element={<CreateBoard />} />,
         label: "팀게시판 생성",
       },
       {
@@ -218,7 +218,7 @@ export const RouterInfo = [
         // path: "apply",
         //백엔드 연결용 동적 주소
         path: "apply/:postId",
-        element: <Apply />,
+        element: <PrivateRoute element={<Apply />} />,
         label: "프로젝트 신청창",
       },
     ],
