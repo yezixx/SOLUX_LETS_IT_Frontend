aaa01572f546f4dcdfe67c149012bc9f0bd64276
yezixx <skylow917@daum.net>
2024-07-30 21:06:51 +0900

    Feat: 이미지 저장 연동 코드 작성

--- src/service/profileService.js
+++ src/service/profileService.js
@@ -6,7 +6,7 @@ export const getProfile = async (userId) => {
     const response = await apiClient.get(`profile/${userId}`);
     return response.data; //성공메세지 반환
   } catch (error) {
-    console.error("Error fetching applicant list:", error);
+    console.error("Error fetching profile:", error);
     throw error; // 에러를 다시 throw하여 호출자에게 전달합니다.
   }
 };
@@ -20,6 +20,27 @@ export const updateProfile = (userId, updatedProfileData) => {
     return response.data;
   } catch (error) {
     console.error("Error updating profile:", error);
+    throw error;
+  }
+};
+
+// 프로필 이미지 저장
+export const saveProfileImage = async (userId, imageFile) => {
+  try {
+    const formData = new FormData();
+    formData.append("profileImage", imageFile);
+    const response = await apiClient.post(
+      `/profile/${userId}/image`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error saving profile image:", error);
     throw error;
   }
 };
