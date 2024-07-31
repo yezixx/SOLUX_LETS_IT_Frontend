77876d9afbb4c187ed7640b1229824e06fb4b024
xxjimin <rr44wlals@gmail.com>
2024-07-31 15:43:14 +0900

    Fix: 메인홈 추천, 인기 프로젝트 컴포넌트에 프로젝트가 하나도 없을 때 문구 추가 및 레이아웃 수정

--- src/Screen/MainHome/RecommendProject/RecommendProject.jsx
+++ src/Screen/MainHome/RecommendProject/RecommendProject.jsx
@@ -3,6 +3,7 @@ import ProjectList from "../../../Components/ProjectList/ProjectList";
 import styles from "./RecommendProject.module.css";
 import { getPostsList } from "../../../service/postService";
 
+
 function RecommendProject() {
   const [projList, setProjList] = useState([]);
   const [sortedList, setSortedList] = useState([]);
@@ -11,34 +12,36 @@ function RecommendProject() {
     const fetchProjects = async () => {
       try {
         const data = await getPostsList();
-        setProjList(data.data);
+        setProjList(data);
       } catch (error) {
-        console.error("Error fetching projects:", error);
+        console.error('Error fetching projects:', error);
       }
     };
 
     fetchProjects();
   }, []);
   useEffect(() => {
-    //조회순 기준 정렬, proList를 받아온 후에 처리함
     if (projList.length > 0) {
       const sorted = [...projList].sort((a, b) => b.viewCount - a.viewCount);
       setSortedList(sorted);
     }
   }, [projList]);
 
+  
+
+
   return (
     <div className={styles.recommendproj}>
       <div className={styles.text}>이런 프로젝트는 어떠신가요? 💫</div>
       <div className={styles.recommedproj__container}>
-        {sortedList.length > 0 ? (
-          sortedList
-            .slice(0, 4)
-            .map((project, index) => (
-              <ProjectList key={index} project={project} />
-            ))
+      {sortedList.length > 0 ? (
+          sortedList.slice(0, 4).map((project, index) => (
+            <ProjectList key={index} project={project} />
+          ))
         ) : (
-          <div>loading</div>
+          <div className={styles.noProjectsMessage}>
+            현재 게시된 프로젝트가 존재하지 않습니다.
+          </div>
         )}
       </div>
     </div>
