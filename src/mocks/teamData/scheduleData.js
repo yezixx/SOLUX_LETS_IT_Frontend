038ab8c7d703fc4f9a9215f37a0744672bd6ccf2
yezixx <skylow917@daum.net>
2024-08-20 21:55:04 +0900

    Feat:  목업데이터 js 파일 생성

---
+++ src/mocks/teamData/scheduleData.js
@@ -0,0 +1,23 @@
+export const scheduleData = [
+  {
+    calendarId: "1",
+    title: "전체회의",
+    startDate: "2024-08-29",
+    endDate: "2024-09-05",
+    description: "저녁 9시에 회의합니다.",
+  },
+  {
+    calendarId: "2",
+    title: "부스팅데이",
+    startDate: "2024-07-29",
+    endDate: "2024-08-05",
+    description: "완성할때까지 숨참기.",
+  },
+  {
+    calendarId: "3",
+    title: "12312313",
+    startDate: "2024-08-10",
+    endDate: "2024-08-10",
+    description: "2151451",
+  },
+];
---
+++ src/mocks/teamData/teamData.js
@@ -0,0 +1,25 @@
+export const teamData = {
+  teamName: "자율주행자동차만들기",
+  notionLink: "testttt.com",
+  githubLink: "test.com",
+  teamMemberInfo: [
+    {
+      position: "Team_Member",
+      userName: "ee",
+      userId: "letsit_backend.model.Member@63bc2163",
+      profileImageUrl: "",
+    },
+    {
+      position: "Team_Member",
+      userName: "bb",
+      userId: "letsit_backend.model.Member@6af9c174",
+      profileImageUrl: "",
+    },
+    {
+      position: "Team_Leader",
+      userName: "한은서",
+      userId: "letsit_backend.model.Member@27249b85",
+      profileImageUrl: "",
+    },
+  ],
+};
